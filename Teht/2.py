87da7d9061143e51d17ebc9452311d93d6641164
Oiva <oiva.paatsema@gmail.com>
2026-02-04 09:22:50 +0200

    Updatet 5 noticed uploded wrong script
    Refactor the code to print a success message when the maximum value is rolled.

--- Teht/2.py
+++ Teht/2.py
@@ -3,20 +3,14 @@ import random
 def hetto(tahk):
     heitto = random.randint(1, tahk)
     return heitto
-#pyytää määrää
+# pyytää määrää
 Syote = int(input("anna maksimi"))
-# heittojen määrät
-nmr=1
-#heiton tulos
-tulos=0
-
+# heiton tulos
+tulos = 0
 # jos heitto ei ole annetu määrä tekee tämän
 while tulos != Syote:
     tulos = hetto(Syote)
-    print(f"heitosta{nmr} tuli {tulos}")
-    # jos tulos ei ollut annetu määrä lisää heiton numeroa
-    if tulos != Syote:
-        nmr = nmr+1
-# jso heitosta tuli annetu määrä pritnaa tämän
-print(f"nopasta tuli maksimi{Syote} äskeisellä heitolla jee :)")
-print(f"heittoja tarvitiin {nmr}")
+    print(f"heitosta tuli {tulos}")
+    if tulos == Syote:
+     print(f"nopasta tuli maksimi{Syote} äskeisellä heitolla jee :)")
+
